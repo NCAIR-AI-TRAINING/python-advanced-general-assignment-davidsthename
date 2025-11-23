cfc39bca6abb7ada240e5f4475c1cce3a39b669d
David <davidikpen07@gmail.com>
2025-11-23 15:22:28 +0100

    Applied duplicate visitor rule

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from datetime import datetime
 import os
 
+
 class DuplicateVisitorError(Exception):
     pass
+
 
 class EarlyEntryError(Exception):
     pass
 
+
 FILENAME = "visitors.txt"
 
+
 def ensure_file():
-    pass
+    if not os.path.exists(FILENAME):
+        with open(FILENAME, "W", encoding="utf-8") as f:
+            pass
+
 
 def get_last_visitor():
-    pass
+    ensure_file()
+
+    with open(FILENAME, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+        if not lines:
+            return None
+        last = lines[-1].split(", ")
+        name = last[0]
+        timestamp = datetime.fromisoformat(last[1])
+    return (name, timestamp)
+    # pass
+
 
 def add_visitor(visitor_name):
-    pass
+    last = get_last_visitor()
+    if last and last[0].lower() == visitor_name.lower():
+        raise DuplicateVisitorError("Can't sign in twice in a row!")
+    # pass
+
 
 def main():
     ensure_file()
@@ -26,6 +49,7 @@ def main():
         print("Visitor added successfully!")
     except Exception as e:
         print("Error:", e)
+
 
 if __name__ == "__main__":
     main()
